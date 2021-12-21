ba733e697c3c1f839cc4252bd1d0f05ed7ddf79b
zeapherine <zeapherine@gmail.com>
2021-12-21 19:34:47 +0530

    reder member data on DAO dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -132,15 +132,39 @@ const App = () => {
 		);
 	}
 
+	// If the user has already claimed their NFT we want to display the interal DAO page to them
+	// only DAO members will see this. Render all the members + token amounts.
 	if (hasClaimedNFT) {
 		return (
 			<div className='member-page'>
 				<h1>🍪DAO Member Page</h1>
 				<p>Congratulations on being a member</p>
+				<div>
+					<div>
+						<h2>Member List</h2>
+						<table className='card'>
+							<thead>
+								<tr>
+									<th>Address</th>
+									<th>Token Amount</th>
+								</tr>
+							</thead>
+							<tbody>
+								{memberList.map((member) => {
+									return (
+										<tr key={member.address}>
+											<td>{shortenAddress(member.address)}</td>
+											<td>{member.tokenAmount}</td>
+										</tr>
+									);
+								})}
+							</tbody>
+						</table>
+					</div>
+				</div>
 			</div>
 		);
 	}
-
 	const mintNft = () => {
 		setIsClaiming(true);
 		// Call bundleDropModule.claim("0", 1) to mint nft to user's wallet.
